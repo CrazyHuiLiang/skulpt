9794f0610cd46457b9c7b4934ac387b6971d06f8
huiliang.zhang <huiliang.zhang@jinshuschool.com>
2025-07-25 11:30:05 +0800

    try to write some demo

---
+++ src/lib/bill/test.js
@@ -0,0 +1,35 @@
+var $builtinmodule = function(name) {
+    console.log("bill.test builtinmodule", {name});
+    var mod = {};
+    function myfact(n) {
+        console.log("myfact called with " + n);
+        if(n < 1) {
+            return 1;
+        } else {
+            return n * myfact(n-1);
+        }
+    }
+    mod.fact = new Sk.builtin.func(function(a) {
+        console.log("bill.test builtin.func", a);
+        return myfact(a.v);  // extract the underlying JS value with .v
+    });
+
+    mod.Stack = Sk.misceval.buildClass(mod, function($gbl, $loc) {
+        console.log("bill.test buildClass Stack class", $gbl, $loc);
+
+        $loc.__init__ = new Sk.builtin.func(function(self) {
+            console.log("bill.test buildClass Stack __init__", self);
+            self.stack = [];
+        });
+        $loc.push = new Sk.builtin.func(function(self,x) {
+            console.log("bill.test buildClass Stack push", self, x);
+            self.stack.push(x);
+        });
+        $loc.pop = new Sk.builtin.func(function(self) {
+            console.log("bill.test buildClass Stack pop", self);
+            return self.stack.pop();
+        });
+    }, "Stack", []);
+
+    return mod;
+};
